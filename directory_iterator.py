2dc94210fd6c399d70fd754a8b3937d3a783f3c6
ChaitanyaVardhan <chaitanyavardhan1@gmail.com>
2018-01-27 19:58:29 +0000

    count files in a directory

--- directory_iterator.py
+++ directory_iterator.py
@@ -1,7 +1,11 @@
 #codes in this files are inspired from the keras source code 
 # https://github.com/keras-team/keras
 
-def count_files_in_directory():
+import os
+import multiprocessing.pool
+from functools import partial
+
+def count_files_in_directory(dirpath, white_list_formats):
     """counts the files of a some format(jpeg, jpg etc.)
     that are present in subdirectories of a directory"""
     def recursive_list(dirpath):
@@ -15,12 +19,28 @@ def count_files_in_directory():
                 if fname.lower().endswith('.' + format):
                     is_valid = True
                     break
-            if is_valid = True:
+            if is_valid == True:
                 count += 1
     return count
 
-if __name__ == '__main__':
-    directory = ''
-    white_list_formats = ['jpeg', 'jpg', 'png']
-    print count_files_in_directory(directory, white_list_formats)
+class CountFiles(object):
+    def __init__(self, directory):
+        self.directory  = directory
+        self.white_list_formats = ['jpeg', 'jpg', 'png']
+        self.valid_subdirs = []
+        self.count = 0
+        for subdir in os.listdir(self.directory):
+            if os.path.isdir(os.path.join(self.directory, subdir)):
+                self.valid_subdirs.append(subdir)
 
+    def count_files(self):
+        function_partial = partial(count_files_in_directory, white_list_formats=self.white_list_formats)
+        pool = multiprocessing.pool.ThreadPool()
+        self.count = sum(pool.map(function_partial, (os.path.join(self.directory, subdir)
+                                                             for subdir in self.valid_subdirs)))
+
+        print 'Found {} files in {} subdirectories '.format(self.count, len(self.valid_subdirs))
+
+if __name__ == '__main__':
+    print count_files_in_directory('/home/ubuntu/dogscats',['jpg', 'jpeg', 'png'])
+    CountFiles('/home/ubuntu/dogscats').count_files()
