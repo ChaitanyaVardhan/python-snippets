1c231087221b3557285ca4870f497005cbcf4881
ChaitanyaVardhan <chaitanyavardhan1@gmail.com>
2018-02-07 12:46:00 +0000

    more tests for iterator

--- cpython_iterator.py
+++ cpython_iterator.py
@@ -54,12 +54,55 @@ class TestIterator():
                 break
             self.res.append(val)
 
+    def check_for_loop(self, it):
+        res = []
+        for i in it:
+            res.append(i)
+        return res
+            
     #Test basic use of iter() function
     def test_iter_basic(self):
         self.check_iterator(iter(range(10)), range(10))
 
+    #test iter(x) and iter(iter(x)) are same
     def test_iter_idempotency(self):
         seq = range(10)
         it = iter(seq)
         it2 = iter(seq)
         return it, it2
+
+    #test that for loop works over iterator
+    def test_iter_for_loop(self):
+        res = self.check_for_loop(iter(range(10)))
+        return res
+
+    #test iter independence over the same list
+    def test_iter_independence(self):
+        seq = range(3)
+        res = []
+        for i in iter(seq):
+            for j in iter(seq):
+                for k in iter(seq):
+                    res.append((i,j,k))
+        return res
+
+    #test nested list comp on iter
+    def test_nested_comp_iter(self):
+        seq = range(3)
+        res = [(i,j,k) for i in iter(seq) for j in iter(seq) for k in iter(seq)]
+        return res
+
+    #test nested comp without iter
+    def test_comp_for(self):
+        seq = range(3)
+        res = [(i,j,k) for i in seq for j in seq for k in seq]
+        return res
+
+    #test for loop for a class with __iter__
+    def test_iter_class_for(self):
+        res = self.check_for_loop(IteratingSequenceClass(10))
+        return res
+
+
+
+
